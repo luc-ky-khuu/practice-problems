1d5981046f1f1910b1485bee598e9858f08d85cf
lkhuu96 <khuu96@gmail.com>
2022-03-11 16:28:30 -0800

    added solution for CodeSignal's array replace problem

--- main.js
+++ main.js
@@ -63,3 +63,13 @@ function solution(matrix) {
     }
     return arrayContainer;
 }
+
+//array replace
+function solution(inputArray, elemToReplace, substitutionElem) {
+    for (var i = 0; i < inputArray.length; i++) {
+        if (inputArray[i] === elemToReplace) {
+            inputArray[i] = substitutionElem;
+        }
+    }
+    return inputArray
+}
