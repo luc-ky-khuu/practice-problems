9cf9b1b3ea67e02ce22ae75769e0284edf380988
lkhuu96 <khuu96@gmail.com>
2022-05-27 10:20:32 -0700

    added codeSignal solution for firstNotRepeatingCharacter

--- codeSignal.js
+++ codeSignal.js
@@ -1,3 +1,22 @@
+// firstNotRepeatingCharacter
+function solution(s) {
+    const obj = {};
+    for (let i = 0; i < s.length; i++) {
+        if (obj[s[i]]) {
+            obj[s[i]] = obj[s[i]] + 1
+        } else {
+            obj[s[i]] = 1
+        }
+    }
+    for (let nums in obj) {
+        if (obj[nums] === 1) {
+            return nums
+        }
+    }
+    return '_'
+}
+
+
 // firstDuplicate
 function solution(a) {
     const obj = {};
